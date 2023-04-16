168de355dfa64cc13e8b8fce847cc9b7f4d0f401
PauloRTrindade <prtrindadesf@gmail.com>
2023-04-16 17:10:09 -0300

    Introduction of interfaces

---
+++ src/service/ContractService.java
@@ -0,0 +1,37 @@
+package service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Contract;
+import entities.Installment;
+
+public class ContractService {
+
+	private OnlinePaymentService onlinePaymentService;
+
+	List<Installment> list = new ArrayList<>();
+
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+
+	public void processContract(Contract contract, int months) {
+		double basicQuota = contract.getTotalValue() / months;
+
+		for (int i = 1; i <= months; i++) {
+
+			LocalDate dueDate = contract.getDate().plusMonths(i);
+
+			double interest = onlinePaymentService.interest(basicQuota, i);
+			double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+			double quota = basicQuota + interest + fee;
+
+			contract.getInstallments().add(new Installment(dueDate, quota));
+
+		}
+
+	}
+
+}
